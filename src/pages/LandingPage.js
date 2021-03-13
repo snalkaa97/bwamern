dd3d5226554f4b60e0c1b30c881ad7e81dbae017
snalkaa97 <sfnalkatiri@gmail.com>
2021-03-13 19:35:14 +0700

    fixing

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
-import modulename from "elements/Button";
+// import modulename from "elements/Button";
+import Header from "parts/Header";
 export default class LandingPage extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<>
+				<Header {...this.props}></Header>
+			</>
+		);
 	}
 }
