83b1857f51e92b50be8fa33b35fbb427274a9368
snalkaa97 <sfnalkatiri@gmail.com>
2021-03-14 22:08:04 +0700

    fixing scss

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,12 +3,14 @@ import React, { Component } from "react";
 import Hero from "parts/Hero";
 import LandingPageJson from "json/landingPage.json";
 import Header from "parts/Header";
+import MostPicked from "parts/MostPicked";
 export default class LandingPage extends Component {
 	render() {
 		return (
 			<>
 				<Header {...this.props}></Header>
 				<Hero data={LandingPageJson.hero} />
+				<MostPicked data={LandingPage.mostPicked} />
 			</>
 		);
 	}
