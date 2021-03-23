7bc2d6af4029e71e09147e76034c0556e372e6b8
snalkaa97 <sfnalkatiri@gmail.com>
2021-03-23 21:07:15 +0700

    componet input number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
+import Example from "pages/Example";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 		<div className="App">
 			<Router>
 				<Route path="/" component={LandingPage}></Route>
+				<Route path="/example" component={Example}></Route>
 			</Router>
 		</div>
 	);
---
+++ src/pages/Example.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import InputNumber from "elements/Form/InputNumber";
+
+export default class Example extends Component {
+	state = {
+		value: "1",
+	};
+
+	handleChange = (e) => {
+		this.setState({ value: e.target.value });
+	};
+
+	render() {
+		return (
+			<div className="container">
+				<div
+					className="row align-item-center justify-content-center"
+					style={{ height: "100vh" }}
+				>
+					<div className="col-auto">
+						<InputNumber
+							max={30}
+							isSuffixPlural
+							suffix={` night`}
+							onChange={this.handleChange}
+							name="value"
+							value={this.state.value}
+						/>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
