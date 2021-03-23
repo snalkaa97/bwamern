779fcd1f3ad5a40d5ed9839a9a0a3b08c093e724
snalkaa97 <sfnalkatiri@gmail.com>
2021-03-23 17:52:37 +0700

    react reveal

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal";
 import ImageHero from "assets/images/img-hero.jpg";
 import ImageHeroFrame from "assets/images/img-hero-frame.jpg";
 // import IconCalendar from "assets/images/icons/icon-calendar.svg";
@@ -15,83 +16,85 @@ export default function Hero(props) {
 	}
 
 	return (
-		<section className="container pt-4">
-			<div className="row align-items-center">
-				<div className="col-auto pr-5" style={{ width: 530 }}>
-					<h1 className="h2 font-weight-bold line-height-1 mb-3">
-						Forget Busy Work, <br />
-						Start Next Vacation
-					</h1>
-					<p className="mb-5 font-weight-light text-gray-500 w-75">
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta
-						tenetur libero dolorem perferendis, iste praesentium ullam, dolore
-						laborum reiciendis, deleniti tempore. Officiis reiciendis nostrum
-						enim. Eum vitae sit itaque eos.
-					</p>
-					<Button
-						className="btn px-5"
-						hasShadow
-						isPrimary
-						onClick={showMostPicked}
-					>
-						Show Me Now
-					</Button>
-					<div className="row mt-5">
-						<div className="col-auto" style={{ marginRight: 35 }}>
-							<img
-								width="36"
-								height="36"
-								src={IconStar}
-								alt={`${props.data.travelers} Travelers`}
-							/>
-							<h6 className="mt-3">
-								{FormatNumber(props.data.travelers)}{" "}
-								<span className="text-gray-500 font-light">Travelers</span>
-							</h6>
+		<Fade bottom>
+			<section className="container pt-4">
+				<div className="row align-items-center">
+					<div className="col-auto pr-5" style={{ width: 530 }}>
+						<h1 className="h2 font-weight-bold line-height-1 mb-3">
+							Forget Busy Work, <br />
+							Start Next Vacation
+						</h1>
+						<p className="mb-5 font-weight-light text-gray-500 w-75">
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta
+							tenetur libero dolorem perferendis, iste praesentium ullam, dolore
+							laborum reiciendis, deleniti tempore. Officiis reiciendis nostrum
+							enim. Eum vitae sit itaque eos.
+						</p>
+						<Button
+							className="btn px-5"
+							hasShadow
+							isPrimary
+							onClick={showMostPicked}
+						>
+							Show Me Now
+						</Button>
+						<div className="row mt-5">
+							<div className="col-auto" style={{ marginRight: 35 }}>
+								<img
+									width="36"
+									height="36"
+									src={IconStar}
+									alt={`${props.data.travelers} Travelers`}
+								/>
+								<h6 className="mt-3">
+									{FormatNumber(props.data.travelers)}{" "}
+									<span className="text-gray-500 font-light">Travelers</span>
+								</h6>
+							</div>
+							<div className="col-auto" style={{ marginRight: 35 }}>
+								<img
+									width="36"
+									height="36"
+									src={IconStar}
+									alt={`${props.data.treasures} Treasures`}
+								/>
+								<h6 className="mt-3">
+									{FormatNumber(props.data.treasures)}{" "}
+									<span className="text-gray-500 font-light">Treasures</span>
+								</h6>
+							</div>
+							<div className="col-auto">
+								<img
+									width="36"
+									height="36"
+									src={IconStar}
+									alt={`${props.data.cities} Cities`}
+								/>
+								<h6 className="mt-3">
+									{FormatNumber(props.data.cities)}{" "}
+									<span className="text-gray-500 font-light">Cities</span>
+								</h6>
+							</div>
 						</div>
-						<div className="col-auto" style={{ marginRight: 35 }}>
+					</div>
+					<div className="col-6 pl-5">
+						<div style={{ width: 520, height: 410 }}>
 							<img
-								width="36"
-								height="36"
-								src={IconStar}
-								alt={`${props.data.treasures} Treasures`}
+								src={ImageHero}
+								alt="Room with couches"
+								className="img-fluid position-absolute"
+								style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
 							/>
-							<h6 className="mt-3">
-								{FormatNumber(props.data.treasures)}{" "}
-								<span className="text-gray-500 font-light">Treasures</span>
-							</h6>
-						</div>
-						<div className="col-auto">
 							<img
-								width="36"
-								height="36"
-								src={IconStar}
-								alt={`${props.data.cities} Cities`}
+								src={ImageHeroFrame}
+								alt="Room with couches frame"
+								className="img-fluid position-absolute"
+								style={{ margin: "0 -15px -15px 0" }}
 							/>
-							<h6 className="mt-3">
-								{FormatNumber(props.data.cities)}{" "}
-								<span className="text-gray-500 font-light">Cities</span>
-							</h6>
 						</div>
 					</div>
 				</div>
-				<div className="col-6 pl-5">
-					<div style={{ width: 520, height: 410 }}>
-						<img
-							src={ImageHero}
-							alt="Room with couches"
-							className="img-fluid position-absolute"
-							style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
-						/>
-						<img
-							src={ImageHeroFrame}
-							alt="Room with couches frame"
-							className="img-fluid position-absolute"
-							style={{ margin: "0 -15px -15px 0" }}
-						/>
-					</div>
-				</div>
-			</div>
-		</section>
+			</section>
+		</Fade>
 	);
 }
--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,43 +1,48 @@
 import Button from "elements/Button";
+import Fade from "react-reveal";
 export default function MostPicked(props) {
 	// console.log(props.data);
 	return (
 		<section className="container" ref={props.refMostPicked}>
-			<h4 className="mb-3">Most Picked</h4>
-			<div className="container-grid">
-				{props.data.map((item, index) => {
-					return (
-						<div
-							key={`mostpicked${index}`}
-							className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
-						>
-							<div className="card card-feature">
-								<div className="tag">${item.price}</div>
-								<span className="font-weight-light">per {item.unit}</span>
+			<Fade bottom>
+				<h4 className="mb-3">Most Picked</h4>
+				<div className="container-grid">
+					{props.data.map((item, index) => {
+						return (
+							<div
+								key={`mostpicked${index}`}
+								className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
+							>
+								<Fade bottom delay={500 * index}>
+									<div className="card card-feature">
+										<div className="tag">${item.price}</div>
+										<span className="font-weight-light">per {item.unit}</span>
+									</div>
+									<figure className="img-wrapper">
+										<img
+											src={item.imageUrl} //memang ga ada gambar
+											alt={item.name}
+											className="img-cover"
+										/>
+									</figure>
+									<div className="meta-wrapper">
+										<Button
+											type="link"
+											className="streched-link d-block text-white"
+											href={`/properties/${item._id}`}
+										>
+											<h5>{item.name}</h5>
+										</Button>
+										<span>
+											{item.city}, {item.country}
+										</span>
+									</div>
+								</Fade>
 							</div>
-							<figure className="img-wrapper">
-								<img
-									src={item.imageUrl} //memang ga ada gambar
-									alt={item.name}
-									className="img-cover"
-								/>
-							</figure>
-							<div className="meta-wrapper">
-								<Button
-									type="link"
-									className="streched-link d-block text-white"
-									href={`/properties/${item._id}`}
-								>
-									<h5>{item.name}</h5>
-								</Button>
-								<span>
-									{item.city}, {item.country}
-								</span>
-							</div>
-						</div>
-					);
-				})}
-			</div>
+						);
+					})}
+				</div>
+			</Fade>
 		</section>
 	);
 }
